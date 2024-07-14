138def8ecc9aa63c2c8a4639c0174aedcec6d151
unknown <leekipil98@gmail.com>
2024-07-14 18:13:26 +0900

    STATIC_ROOT 생성

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,6 @@
 from .base import *
 
 ALLOWED_HOSTS = ['13.125.35.45'] # AWS lightsail public ip 
+STATIC_ROOT = BASE_DIR / 'static/' 
+# /home/ubuntu/projects/mysite/static를 nginx 정적파일 위치로 등록
+STATICFILES_DIRS = [] # base.py와 충돌 방지를 피하고자 override
